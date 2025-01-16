10f990f6d4fef92d0f948af05259f42634f2d38e
IssamElJaouhari <issamefar58@gmail.com>
2025-01-16 17:33:30 +0100

    add MongoDB dependency and implement CRUD operations with Mongoose

---
+++ server/crudMongo.js
@@ -0,0 +1,74 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+
+const url_db = process.env.DATA_BASE_URL
+
+
+mongoose.connect(url_db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => {
+    console.log('Connected to MongoDB successfully!');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+  });
+
+
+  //schema of our collection
+  const schemaPerson = new mongoose.Schema(
+    {
+        name:{
+            type: String, 
+            required: true
+        },
+        age:Number,
+        favoriteFoods:[String]
+    }
+  );
+
+const Person = mongoose.model('Person', schemaPerson)
+
+
+const createPerson = (done) =>{
+    const person = new Person({
+        name:"Issam",
+        age:30,
+        favoriteFoods:["rfissa", "briwa", "toplexil","water"]
+    })
+    person.save((req, res)=>{
+        if (err) return done(err)
+            done(null, data)
+    })
+}
+//create many  records with model.create , we use peson because person it hold the  [mongoose.model]
+const createManyPeople = (arrayOfppl,done)=>{
+    Person.create(arrayOfppl,(err, data)=>{
+        if(err) return done(err)
+            done(null, data)
+    })
+}
+
+const findPeopleByNam = (namePerson,done)=>{
+    Person.find({name:namePerson},(err, data)=>{
+        if(err) return done(err)
+            done(null, data)
+    })
+}
+
+const findPersonByFood= (food,done)=>{
+    Person.findOne({favoriteFoods:food},(err, data)=>{
+        if(err) return done(err)
+            done(null, data)
+    })
+}
+
+const findPersonById= (personId,done)=>{
+    Person.findById(personId,(err, data)=>{
+        if(err) return done(err)
+            done(null, data)
+    })
+}
+
+
+// ....to be continued
